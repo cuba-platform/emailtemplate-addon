049055e500b35eafe3325bea3779bd711227fd2d
Alexey Nikotin <nikotin@haulmont.com>
2018-04-20 18:47:50 +0400

    YARG_ET-3 Create the editor screens for ContentEmailTemplate and LayoutEmailTemplate entities

---
+++ modules/web/src/com/haulmont/addon/yargemailtemplateaddon/web/contentemailtemplate/ContentEmailTemplateEdit.java
@@ -0,0 +1,7 @@
+package com.haulmont.addon.yargemailtemplateaddon.web.contentemailtemplate;
+
+import com.haulmont.cuba.gui.components.AbstractEditor;
+import com.haulmont.addon.yargemailtemplateaddon.entity.ContentEmailTemplate;
+
+public class ContentEmailTemplateEdit extends AbstractEditor<ContentEmailTemplate> {
+}
---
+++ modules/web/src/com/haulmont/addon/yargemailtemplateaddon/web/layoutemailtemplate/LayoutEmailTemplateEdit.java
@@ -0,0 +1,7 @@
+package com.haulmont.addon.yargemailtemplateaddon.web.layoutemailtemplate;
+
+import com.haulmont.cuba.gui.components.AbstractEditor;
+import com.haulmont.addon.yargemailtemplateaddon.entity.LayoutEmailTemplate;
+
+public class LayoutEmailTemplateEdit extends AbstractEditor<LayoutEmailTemplate> {
+}
