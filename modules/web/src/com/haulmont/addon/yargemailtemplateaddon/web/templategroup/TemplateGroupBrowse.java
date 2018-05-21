8d588f443acf4249ce429d7d4f1c3e9a76fb96e1
Alexey Nikotin <nikotin@haulmont.com>
2018-05-21 14:42:06 +0400

    YARG_ET-7 Create template group entity

---
+++ modules/web/src/com/haulmont/addon/yargemailtemplateaddon/web/templategroup/TemplateGroupBrowse.java
@@ -0,0 +1,6 @@
+package com.haulmont.addon.yargemailtemplateaddon.web.templategroup;
+
+import com.haulmont.cuba.gui.components.AbstractLookup;
+
+public class TemplateGroupBrowse extends AbstractLookup {
+}
---
+++ modules/web/src/com/haulmont/addon/yargemailtemplateaddon/web/templategroup/TemplateGroupEdit.java
@@ -0,0 +1,7 @@
+package com.haulmont.addon.yargemailtemplateaddon.web.templategroup;
+
+import com.haulmont.cuba.gui.components.AbstractEditor;
+import com.haulmont.addon.yargemailtemplateaddon.entity.TemplateGroup;
+
+public class TemplateGroupEdit extends AbstractEditor<TemplateGroup> {
+}
