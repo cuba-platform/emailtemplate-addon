ed95fcf29cf685b2f4950f9059df85de01077de7
Alexey Nikotin <nikotin@haulmont.com>
2018-08-17 15:28:25 +0400

    Unnecessary create and update scripts removing

--- modules/core/db/update/hsql/18/180427-1-createYetContentEmailTemplateReportLink.sql
+++
@@ -1,5 +0,0 @@
-create table YET_CONTENT_EMAIL_TEMPLATE_REPORT_LINK (
-    CONTENT_EMAIL_TEMPLATE_ID varchar(36) not null,
-    REPORT_ID varchar(36) not null,
-    primary key (CONTENT_EMAIL_TEMPLATE_ID, REPORT_ID)
-);
--- modules/core/db/update/hsql/18/180615-2-updateLayoutEmailTemplate.sql
+++
@@ -1,2 +0,0 @@
--- update YET_LAYOUT_EMAIL_TEMPLATE set REPORT_ID = <default_value> where REPORT_ID is null ;
-alter table YET_LAYOUT_EMAIL_TEMPLATE alter column REPORT_ID set not null ;
