9931764ac5b4d1f5fe8a5ea7ff216fa245b1b403
Alexey Nikotin <nikotin@haulmont.com>
2018-09-12 18:46:51 +0400

    fis tests

--- modules/core/test/com/haulmont/addon/emailtemplates/core/EmailTemplatesAPITest.groovy
+++ modules/core/test/com/haulmont/addon/emailtemplates/core/EmailTemplatesAPITest.groovy
@@ -3,8 +3,12 @@ package com.haulmont.addon.emailtemplates.core
 import com.haulmont.addon.emailtemplates.EmailtemplatesTestContainer
 import com.haulmont.addon.emailtemplates.dto.ReportWithParams
 import com.haulmont.addon.emailtemplates.entity.EmailTemplate
+import com.haulmont.addon.emailtemplates.entity.ParameterValue
+import com.haulmont.addon.emailtemplates.exceptions.ReportParameterTypeChangedException
 import com.haulmont.addon.emailtemplates.exceptions.TemplateNotFoundException
 import com.haulmont.cuba.core.global.AppBeans
+import com.haulmont.reports.entity.ParameterType
+import com.haulmont.reports.entity.ReportInputParameter
 import org.junit.ClassRule
 import spock.lang.Shared
 import spock.lang.Specification
@@ -18,13 +22,23 @@ class EmailTemplatesAPITest extends Specification {
     Map<String, Object> paramsMap
     @Shared
     List<ReportWithParams> paramsList
+    @Shared
+    ReportInputParameter parameter
+    @Shared
+    ParameterValue value
 
+    @Shared
     EmailTemplatesAPI delegate
 
-    void setup() {
+    void setupSpec() {
         delegate = AppBeans.get(EmailTemplates)
         paramsMap = new HashMap<>()
         paramsList = new ArrayList<>()
+
+        parameter = new ReportInputParameter()
+        parameter.setType(ParameterType.ENTITY)
+        value = new ParameterValue()
+        value.setParameterType(ParameterType.TEXT)
     }
 
     def "check that method generateEmail with list params throw exception with empty template"() {
@@ -55,6 +69,19 @@ class EmailTemplatesAPITest extends Specification {
         new EmailTemplate()  | paramsMap
         null                 | null
         new EmailTemplate()  | null
+    }
+
+    def "check that method checkParameterTypeChanged throw exception if parameter type was changed"() {
+        when:
+        delegate.checkParameterTypeChanged(inputParameter, parameterValue)
+
+        then:
+        thrown(ReportParameterTypeChangedException)
+
+        where:
+        inputParameter             | parameterValue
+        new ReportInputParameter() | new ParameterValue()
+        parameter                  | new ParameterValue()
     }
 
 }
