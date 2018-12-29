fed2b742f9af9141d69aeb08381e8c4fcc5f4ab8
Alexey Nikotin <nikotin@haulmont.com>
2018-12-29 12:03:06 +0400

    YARG_ET-149 fix TemplateGroup screens

--- modules/web/src/com/haulmont/addon/emailtemplates/web/templategroup/TemplateGroupBrowse.java
+++ modules/web/src/com/haulmont/addon/emailtemplates/web/templategroup/TemplateGroupBrowse.java
@@ -1,10 +1,12 @@
 package com.haulmont.addon.emailtemplates.web.templategroup;
 
 import com.haulmont.cuba.gui.screen.*;
+import com.haulmont.addon.emailtemplates.entity.TemplateGroup;
+
 
 @UiController("emailtemplates$TemplateGroup.browse")
 @UiDescriptor("template-group-browse.xml")
 @LookupComponent("templateGroupsTable")
 @LoadDataBeforeShow
-public class TemplateGroupBrowse extends StandardLookup {
+public class TemplateGroupBrowse extends StandardLookup<TemplateGroup> {
 }
--- modules/web/src/com/haulmont/addon/emailtemplates/web/templategroup/TemplateGroupEdit.java
+++ modules/web/src/com/haulmont/addon/emailtemplates/web/templategroup/TemplateGroupEdit.java
@@ -1,15 +1,12 @@
 package com.haulmont.addon.emailtemplates.web.templategroup;
 
+import com.haulmont.cuba.gui.screen.*;
 import com.haulmont.addon.emailtemplates.entity.TemplateGroup;
-import com.haulmont.cuba.gui.components.AbstractEditor;
-import com.haulmont.cuba.gui.screen.EditedEntityContainer;
-import com.haulmont.cuba.gui.screen.LoadDataBeforeShow;
-import com.haulmont.cuba.gui.screen.UiController;
-import com.haulmont.cuba.gui.screen.UiDescriptor;
+
 
 @UiController("emailtemplates$TemplateGroup.edit")
 @UiDescriptor("template-group-edit.xml")
 @EditedEntityContainer("templateGroupDc")
 @LoadDataBeforeShow
-public class TemplateGroupEdit extends AbstractEditor<TemplateGroup> {
+public class TemplateGroupEdit extends StandardEditor<TemplateGroup> {
 }
