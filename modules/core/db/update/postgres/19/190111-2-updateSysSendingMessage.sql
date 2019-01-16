39b0457796dc73cab05d786aa9ef9be30d7eee98
zaharchenko <zaharchenko@haulmont.com>
2019-01-16 09:28:10 +0400

    add postgres update scripts

---
+++ modules/core/db/update/postgres/19/190111-2-updateSysSendingMessage.sql
@@ -0,0 +1,3 @@
+alter table SYS_SENDING_MESSAGE add column ADDRESS_CC varchar(255) ;
+alter table SYS_SENDING_MESSAGE add column ADDRESS_BCC varchar(255) ;
+alter table SYS_SENDING_MESSAGE add column DTYPE varchar(100) ;
